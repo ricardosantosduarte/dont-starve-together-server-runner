d5fedd676c04d65a915ed234678812b77d022a9d
ricardosantosduarte <ricardosantosduarte1999@gmail.com>
2023-08-02 18:03:31 +0100

    Adicionar arquivos de projeto.

---
+++ DontStarveServerRunner/DontStarveServerRunner.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace DontStarveServerRunner
+{
+    partial class DontStarveServerRunner
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStartServer = new System.Windows.Forms.Button();
+            this.buttonStopServer = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelServerStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonStartServer
+            // 
+            this.buttonStartServer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonStartServer.Location = new System.Drawing.Point(12, 309);
+            this.buttonStartServer.Name = "buttonStartServer";
+            this.buttonStartServer.Size = new System.Drawing.Size(301, 23);
+            this.buttonStartServer.TabIndex = 0;
+            this.buttonStartServer.Text = "Start Server";
+            this.buttonStartServer.UseVisualStyleBackColor = true;
+            this.buttonStartServer.Click += new System.EventHandler(this.buttonStartServer_Click);
+            // 
+            // buttonStopServer
+            // 
+            this.buttonStopServer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonStopServer.Location = new System.Drawing.Point(12, 365);
+            this.buttonStopServer.Name = "buttonStopServer";
+            this.buttonStopServer.Size = new System.Drawing.Size(301, 23);
+            this.buttonStopServer.TabIndex = 1;
+            this.buttonStopServer.Text = "Stop Server";
+            this.buttonStopServer.UseVisualStyleBackColor = true;
+            this.buttonStopServer.Click += new System.EventHandler(this.buttonStopServer_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(83, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Server Status:";
+            // 
+            // labelServerStatus
+            // 
+            this.labelServerStatus.AutoSize = true;
+            this.labelServerStatus.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.labelServerStatus.Location = new System.Drawing.Point(173, 71);
+            this.labelServerStatus.Name = "labelServerStatus";
+            this.labelServerStatus.Size = new System.Drawing.Size(0, 13);
+            this.labelServerStatus.TabIndex = 3;
+            // 
+            // DontStarveServerRunner
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 450);
+            this.Controls.Add(this.labelServerStatus);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonStopServer);
+            this.Controls.Add(this.buttonStartServer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "DontStarveServerRunner";
+            this.Text = "Dont Starve Together Server Runner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStartServer;
+        private System.Windows.Forms.Button buttonStopServer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelServerStatus;
+    }
+}
+
---
+++ DontStarveServerRunner/DontStarveServerRunner.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DontStarveServerRunner
+{
+    public partial class DontStarveServerRunner : Form
+    {
+        public DontStarveServerRunner()
+        {
+            InitializeComponent();
+            labelServerStatus.Text = "Offline";
+            labelServerStatus.ForeColor = Color.Red;
+        }
+
+        [DllImport("user32.dll")]
+        internal static extern IntPtr SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        internal static extern bool ShowWindow(IntPtr hWnd, int nCmdShow); //ShowWindow needs an IntPtr
+
+        [DllImport("kernel32.dll")]
+        static extern bool AttachConsole(int dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        static extern bool FreeConsole();
+
+        private void buttonStartServer_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process process = Process.Start("C:\\Users\\ricar\\Documents\\Klei\\DoNotStarveTogether\\StartDSTServer.bat");
+                process.WaitForExit();
+
+                labelServerStatus.Text = "Online";
+                labelServerStatus.ForeColor = Color.YellowGreen;
+            } catch (Exception)
+            {
+                labelServerStatus.Text = "Offline";
+                labelServerStatus.ForeColor = Color.Red;
+            }
+        }
+
+        private void buttonStopServer_Click(object sender, EventArgs e)
+        {
+            IntPtr hWnd; //change this to IntPtr
+            Process[] processRunning = Process.GetProcesses();
+            foreach (Process pr in processRunning)
+            {
+                if (pr.ProcessName == "dontstarve_dedicated_server_nullrenderer_x64")
+                {
+                    try
+                    {
+                        hWnd = pr.MainWindowHandle; //use it as IntPtr not int
+                        ShowWindow(hWnd, 3);
+                        SetForegroundWindow(hWnd);
+
+                        SendKeys.Send("c_shutdown{(}{)}{ENTER}");
+                        SendKeys.Flush();
+                    } finally
+                    {
+                        labelServerStatus.Text = "Offline";
+                        labelServerStatus.ForeColor = Color.Red;
+                    }
+                }
+            }
+        }
+    }
+}
